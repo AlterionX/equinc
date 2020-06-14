3c5b60906c8936b27e048c72b7ee47ab1cfcec0a
Benjamin Xu <ben.xu.cs@gmail.com>
2020-06-13 21:39:06 -0500

    Add ApproxRatio

--- src/util.rs
+++ src/util.rs
@@ -11,3 +11,15 @@ where
     let (numer, denom): (I, I) = input.into();
     Ratio::new(numer.into(), denom.into())
 }
+
+pub struct ApproxRatio<T>(pub Ratio<T>);
+
+impl<T: Clone + std::fmt::Display + Integer + From<u8>> std::fmt::Display for ApproxRatio<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let r = self.0.clone();
+        let ratio_100: Ratio<T> = Ratio::from_integer(100u8.into());
+        let trunc = r.to_integer();
+        let fract = (r.fract() * ratio_100.clone() % ratio_100).to_integer();
+        write!(f, "{}.{:02} (approx)", trunc, fract)
+    }
+}
